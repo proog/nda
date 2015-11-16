9274c50f7df005e040e50b1d16f8d10c775e4581
Per Mortensen <per@permortensen.com>
2015-11-16 20:19:25 +0100

    improved idle talk interval and delay

--- idle_talk.py
+++ idle_talk.py
@@ -4,18 +4,20 @@ import os.path
 
 
 class IdleTalk:
-    log = []
-    last_message = time.time()
-    last_generated_message = 0
-
     min_log_length = 30
     max_log_length = 200
     log_file = 'idle_talk.log'
 
     min_idle_delay = 1800  # 30 mins
     max_idle_delay = 7200  # 2 hours
-    min_message_interval = 15
+    min_message_interval = 5
     max_message_interval = 1800
+
+    def __random_delay(self):
+        return random.randint(self.min_idle_delay, self.max_idle_delay)
+
+    def __random_interval(self):
+        return random.randint(self.min_message_interval, self.max_message_interval)
 
     def __trim_log(self):
         while len(self.log) > self.max_log_length:
@@ -25,6 +27,7 @@ class IdleTalk:
     def add_message(self, msg):
         self.log.append(msg)
         self.last_message = time.time()
+        self.delay = self.__random_delay()  # set a random delay for each "quiet period"
 
         with open(self.log_file, 'a') as file:
             file.write('%s\r\n' % msg)
@@ -33,18 +36,24 @@ class IdleTalk:
 
     def can_talk(self):
         t = time.time()
-        random_delay = random.randint(self.min_idle_delay, self.max_idle_delay)
-        random_interval = random.randint(self.min_message_interval, self.max_message_interval)
 
         return len(self.log) >= self.min_log_length \
-            and self.last_message + random_delay < t \
-            and self.last_generated_message + random_interval < t
+            and self.last_message + self.delay < t \
+            and self.last_generated_message + self.interval < t
 
     def generate_message(self):
         self.last_generated_message = time.time()
+        self.interval = self.__random_interval()  # set a random interval after each generated message
+
         return self.log[random.randint(0, len(self.log))]
 
     def __init__(self):
+        self.log = []
+        self.last_message = time.time()
+        self.last_generated_message = 0
+        self.delay = self.__random_delay()
+        self.interval = self.__random_interval()
+
         if os.path.isfile(self.log_file):
             with open(self.log_file) as file:
                 self.log = file.readlines()
