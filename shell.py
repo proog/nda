197a71776fefae5788162ff8ee89af95675a3b9f
Per Mortensen <per@permortensen.com>
2015-11-19 18:45:13 +0100

    backwards compatibility for subprocess

--- shell.py
+++ shell.py
@@ -4,18 +4,16 @@ import sys
 
 
 def run(args):
-    process = subprocess.run(args, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    output = process.stdout.decode('utf-8').splitlines()
+    output = subprocess.check_output(args, shell=True, stderr=subprocess.STDOUT).decode('utf-8').splitlines()
 
     if len(output) == 0:
-        output = ['return code %i, no output' % process.returncode] + output
+        output = ['no output']
 
     return output
 
 
 def git_pull():
-    process = subprocess.run(['git', 'pull'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    return process.returncode == 0
+    return subprocess.check_call(['git', 'pull']) == 0
 
 
 def restart(file):
